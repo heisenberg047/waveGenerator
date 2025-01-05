1f7eaad9e5cde1ad736302052ed7f9db724fac41
Your Name <your-email@example.com>
2025-01-05 15:49:12 +0530

    updated error logic

--- createVcdFile.js
+++ createVcdFile.js
@@ -1,39 +1,53 @@
-const fs = require('fs');
-const convertVcdToJson = require('./vcdModule/app')
-const convertToWaveDrom = require('./vcdModule/wavedrom')
-const vcdFile = 'dump.vcd';
-const outpath = 'output.json'
-// const multer = require('multer')
-const path = require('path');
-const { exec } = require('child_process');
-const util = require('util');
+const fs = require("fs");
+const convertVcdToJson = require("./vcdModule/app");
+const convertToWaveDrom = require("./vcdModule/wavedrom");
+const vcdFile = "dump.vcd";
+const outpath = "output.json";
+const path = require("path");
+const { exec } = require("child_process");
+const util = require("util");
 const execAsync = util.promisify(exec);
 
 const CreateVcdFile = async (req, res) => {
-  req.body["files"].forEach(element => {
+  req.body["files"]?.forEach((element) => {
     fs.writeFile(element.filename, element.content, (err) => {
       if (err) {
-        console.error('Error writing to file:', err);
+        console.error("Error writing to file:", err);
         return;
       }
-      console.log('File created and content written successfully!');
+      console.log("File created and content written successfully!");
     });
   });
 
+  if (req.body["files"]) {
+    let cmd2;
+    let cmd1 = await runCmd("iverilog -o fa_sim fa.v fa_tb.v");
+    if (cmd1.procced) {
+      cmd2 = await runCmd("vvp fa_sim");
+    } else {
+      res.send({ success: false, message: cmd1.error });
+      return;
+    }
+
+    if (cmd2.procced) {
+      convertVcdToJson(vcdFile, outpath);
+      convertToWaveDrom(outpath, "wavedrom.json");
+
+      req.body["files"]?.forEach((element) => {
+        let pathname = path.join(__dirname, "", element.filename);
+        fs.unlinkSync(pathname);
+      });
+    } else {
+      res.send({ success: false, message: cmd2.error });
+    }
+  } else {
+    res.send({ success: false, message: "Files not sent" });
+  }
 
 
-  await runCmd('iverilog -o fa_sim fa.v fa_tb.v')
-  await runCmd('vvp fa_sim')
-
-  convertVcdToJson(vcdFile, outpath);
-  convertToWaveDrom(outpath, "wavedrom.json")
-
-
-  const filePath = path.join(__dirname, '', 'wavedrom.json');
-  res.sendFile(filePath);
-
-
-}
+      const filePath = path.join(__dirname, "", "wavedrom.json");
+      res.sendFile(filePath);
+};
 
 async function runCmd(cmd) {
   try {
@@ -41,18 +55,14 @@ async function runCmd(cmd) {
 
     if (stderr) {
       console.error(`Stderr: ${stderr}`);
-      return;
+      return { procced: false, error: stderr };
     }
 
     console.log(`Output: ${stdout}`);
+    return { procced: true, error: "" };
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    return { procced: false, error: error.message };
   }
 }
 
 module.exports = CreateVcdFile;
-
-
-
-
-
